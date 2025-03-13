b4bc8b765887be0d7ba96cae84730c65a6bbc16e
Patrick Stephens <patsteph@cisco.com>
2025-03-12 21:22:07 -0500

    Updates

---
+++ _next/_buildManifest.js
@@ -0,0 +1,11 @@
+self.__BUILD_MANIFEST = {
+  "polyfillFiles": [],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [],
+  "pages": {
+    "/": []
+  },
+  "ampFirstPages": []
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ personalsite/_next/_ssgManifest.js
@@ -0,0 +1,3 @@
+self.__SSG_MANIFEST = {
+  "pages": {}
+};self.__SSG_MANIFEST_CB && self.__SSG_MANIFEST_CB()
